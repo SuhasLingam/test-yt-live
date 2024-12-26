bad42d06ab584215879afe189192d17f6f8df8ec
SuhasLingam <95020208+SuhasLingam@users.noreply.github.com>
2024-12-26 22:15:17 +0530

    ...

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -14,7 +14,7 @@ export function Leaderboard() {
   const { videoId } = usePollContext();
   const { data: leaderboardData } = api.leaderboard.getTopUsers.useQuery(
     { 
-      limit: 5,
+      limit: 10,
       videoId: videoId ?? undefined
     },
     { 
